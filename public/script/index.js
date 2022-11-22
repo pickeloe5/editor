ece54e1a690861b68c6c326b090f9e3844275b9f
Nick Mattiacci <aboutthepickle@gmail.com>
2022-11-22 04:10:33 -0500

    Added simple cursor.

--- public/script/index.js
+++ public/script/index.js
@@ -3,43 +3,102 @@ import {ComponentBase} from './fuffle.js'
 const PRINTABLE = /^[a-zA-Z0-9`~!@#$%^&*()\-_=+\[\]{}\\|;:'",.<>\/?\s]$/
 
 class EditorConsole extends ComponentBase {
+
   static TAG_NAME = 'editor-console'
   static create(text) {
     const instance = this.fromElement(document.createElement(this.TAG_NAME))
     return text ? instance.withText(text) : instance
   }
   static Element = EditorConsole.defineElement('editor-console')
+
   #element = null
-  #text = ''
   #eventTarget = new EventTarget()
+
+  #text = ''
+  #textNodes = [document.createTextNode(''), document.createTextNode('')]
+  #isFocused = false
+  #cursor = 0
+  #cursorNode = document.createElement('span')
+
   constructor($) {
     super($)
+
+    this.#cursorNode.classList.add('editor-cursor')
+    $.appendChild(this.#textNodes[0])
+
     this.#element = $
+    this.#bake()
     $.addEventListener('keydown', _=>{this.#onKeyDown(_)})
     $.addEventListener('keypress', _=>{this.#onKeyPress(_)})
+    $.addEventListener('blur', _=>{this.#onBlur(_)})
+    $.addEventListener('focus', _=>{this.#onFocus(_)})
     $.setAttribute('tabindex', '0')
     $.focus()
-    this.withText()
   }
-  withText(text = this.#text) {
-    this.#text = text
-    if (!text?.length || text.endsWith('\n'))
-      text += ' '
-    this.#element.textContent = text
-    return this
-  }
+
   insertCharacter(character) {
-    return this.withText(this.#text + character)
+    if (!this.#isFocused)
+      return;
+    this.#text =
+      this.#text.substring(0, this.#cursor) +
+      character +
+      this.#text.substring(this.#cursor)
+    this.#cursor++
+    this.#bake()
   }
+
   removeCharacter() {
-    return !this.#text.length ? this :
-      this.withText(this.#text.substring(0, this.#text.length - 1))
+    if (!this.#isFocused)
+      return;
+    this.#text =
+      this.#text.substring(0, this.#cursor - 1) +
+      this.#text.substring(this.#cursor)
+    this.#cursor--
+    this.#bake()
   }
+
+  #bake() {
+    if (this.#isFocused) {
+      this.#textNodes[1].textContent = this.#text.substring(this.#cursor + 1)
+      this.#cursorNode.textContent = this.#text[this.#cursor] || ' '
+      this.#textNodes[0].textContent = this.#text.substring(0, this.#cursor)
+      if (!this.#cursorNode.parentNode) {
+        this.#element.appendChild(this.#cursorNode)
+        this.#element.appendChild(this.#textNodes[1])
+      }
+      return;
+    }
+    if (this.#cursorNode.parentNode) {
+      this.#element.removeChild(this.#textNodes[1])
+      this.#element.removeChild(this.#cursorNode)
+    }
+    this.#textNodes[0].textContent = this.#text
+  }
+
   #dispatch(nativeEvent) {
     const e = new Event(nativeEvent.type, {nativeEvent})
     this.#eventTarget.dispatchEvent(e)
     return e
   }
+
+  #onFocus(e) {
+    if (this.#dispatch(e).defaultPrevented)
+      return;
+    if (this.#isFocused)
+      return;
+    this.#isFocused = true
+    this.#bake()
+  }
+
+  #onBlur(e) {
+    if (this.#dispatch(e).defaultPrevented)
+      return;
+    if (!this.#isFocused)
+      return;
+    this.#isFocused = false
+    this.#bake()
+  }
+
   #onKeyDown(e) {
     if (this.#dispatch(e).defaultPrevented)
       return;
@@ -47,8 +106,25 @@ class EditorConsole extends ComponentBase {
       case 'Backspace':
         this.removeCharacter()
         return;
+      case 'ArrowLeft':
+        if (!this.#isFocused || !this.#cursor)
+          return;
+        this.#cursor--
+        this.#bake()
+        return;
+      case 'ArrowRight':
+        if (!this.#isFocused || this.#cursor >= this.#text.length)
+          return;
+        this.#cursor++
+        this.#bake()
+        return;
+      case 'Tab':
+        this.insertCharacter('\t')
+        e.preventDefault()
+        return;
     }
   }
+
   #onKeyPress(e) {
     if (this.#dispatch(e).defaultPrevented)
       return;
@@ -62,4 +138,5 @@ class EditorConsole extends ComponentBase {
         return;
     }
   }
+
 }
